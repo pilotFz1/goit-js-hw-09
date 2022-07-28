88936caa5e2ada4484d63c59636c09aa5061acbd
Roma Romanenko <romanenko.roma92@gmail.com>
2022-07-28 21:47:39 +0300

    completed homework

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -54,16 +54,21 @@ function onClickBtn() {
   function timer() {
     let today = new Date();
     delta = deadLine - today - 1;
-
     const second = pad(Math.floor(delta / 1000) % 60);
     const minute = pad(Math.floor(delta / 1000 / 60) % 60);
     const hour = pad(Math.floor(delta / 1000 / 60 / 60) % 24);
     const day = pad(Math.floor(delta / 1000 / 60 / 60 / 24));
-
-    daysEl.textContent = day;
-    hoursElement.textContent = hour;
-    minutesElement.textContent = minute;
-    secondsElement.textContent = second;
+    if (second >= 0 || minute >= 0 || hour >= 0 || day >= 0) {
+      daysEl.textContent = day;
+      hoursElement.textContent = hour;
+      minutesElement.textContent = minute;
+      secondsElement.textContent = second;
+    } else {
+      daysEl.textContent = '00';
+      hoursElement.textContent = '00';
+      minutesElement.textContent = '00';
+      secondsElement.textContent = '00';
+    }
     /* console.log(second);
     console.log(minute);
     console.log(hour);
--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,8 +1,93 @@
+import Notiflix from 'notiflix';
+
+const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+};
+
+refs.form.addEventListener('submit', onSubmitForm);
+
+function onSubmitForm(evt) {
+  evt.preventDefault();
+  const amount = refs.amount.value;
+  const delay = refs.delay.value;
+  const step = refs.step.value;
+
+  for (let i = 1; i <= amount; i += 1) {
+    setTimeout(() => {
+      const position = i;
+      const newdelay = (i - 1) * step + +delay;
+      createPromise(position, newdelay)
+        .then(value => {
+          Notiflix.Notify.success(`${value}`);
+        })
+        .catch(error => {
+          Notiflix.Notify.failure(`${error}`);
+        });
+    }, i * step);
+  }
+  evt.target.reset();
+}
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  console.log(delay);
+  const promise = new Promise((resolve, reject) => {
+    console.log(position);
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
+  return promise;
 }
+
+/* Емитация введеных данных */
+/* const amount = 5;
+const position = 0;
+const firstDelay = 1000;
+const delayStep = 1500;
+const delay = 0; */
+
+/* const refs = {
+  form: document.querySelector('.form'),
+  delay: document.querySelector('input[name=delay]'),
+  step: document.querySelector('input[name=step]'),
+  amount: document.querySelector('input[name=amount]'),
+}; */
+
+/* setInterval(test, delayStep); */
+/* function createPromise(position, delay) {
+  return new Promise((fulfill, reject) => {
+    for (let i = 0; i < amount; i++) {
+      position++;
+      delay += delayStep;
+
+      setTimeout(() => {
+        const shouldResolve = Math.random() > 0.3;
+        if (shouldResolve) {
+          fulfill(
+            console.log(`✅ Fulfilled promise ${position} in ${delay}ms`)
+          );
+        } else {
+          reject(console.log(`❌ Rejected promise ${position} in ${delay}ms`));
+        }
+      }, delayStep);
+    }
+  });
+}
+
+createPromise(position, delay)
+  .then(sacses => {
+    sacses;
+  })
+
+  .catch(error => {
+    error;
+  });
+ */
