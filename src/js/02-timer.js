bab19f5744552635d0dc177bd729432ac4e73d02
Roma Romanenko <romanenko.roma92@gmail.com>
2022-07-29 13:34:39 +0300

    fix

--- src/js/02-timer.js
+++ src/js/02-timer.js
@@ -50,7 +50,7 @@ function onClickBtn() {
 
   function timer() {
     let today = new Date();
-    delta = deadLine - today;
+    let delta = deadLine - today;
     const second = pad(Math.floor(delta / 1000) % 60);
     const minute = pad(Math.floor(delta / 1000 / 60) % 60);
     const hour = pad(Math.floor(delta / 1000 / 60 / 60) % 24);
